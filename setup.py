281d9cda650d2ec718d86c536aabd926ac500640
Max Halford <maxhalford25@gmail.com>
2026-04-03 23:43:15 +0200

    try this

--- setup.py
+++ setup.py
@@ -10,10 +10,7 @@ extensions = cythonize([
         include_dirs=[np.get_include()],
         define_macros=[
             ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION"),
-            ("Py_LIMITED_API", "0x030B0000"),
-            ("CYTHON_LIMITED_API", "0x030B0000"),
         ],
-        py_limited_api=True,
     )
 ],
 compiler_directives={"language_level": 2})
@@ -21,6 +18,5 @@ compiler_directives={"language_level": 2})
 # setuptools will read metadata from pyproject.toml
 setup(
     ext_modules=extensions,
-    options={"bdist_wheel": {"py_limited_api": "cp311"}},
     package_data={"vose": ["*.pxd"]},
 )
